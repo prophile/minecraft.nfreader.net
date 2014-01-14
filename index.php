065203ee2ffbb7cc2f3d3d77655d5bcdd35c9914
Nick Farley <nick@nfreader.net>
2014-01-14 14:59:48 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,83 @@
+<?php require_once('header.php'); ?>
+  	<div class="section-header" id="rules">
+  		<h1>The Rules</h1>
+  		<p>The Law of the Land is intended to be as fair as possible. However, players that fail to adhere to these guidelines will be banned from playing on this server.</p>
+	</div>
+	<dl>
+		<dt><div class="num">1</div>Do not be a dick</dt>
+		<dd>Observe <a href="http://www.urbandictionary.com/define.php?term=Wheaton%27s%20Law" target="_blank">Wheaton&apos;s Law</a> at all times.</dd>
+		
+		<dt><div class="num">2</div> Do not grief</dt>
+		<dd>If you didn't build it, don't touch it without asking permission.</dd>
+		
+		<dt><div class="num">3</div> Do not steal</dt>
+		<dd>Same as before.</dd>
+		
+		<dt><div class="num">4</div> Do not use client side mods</dt>
+		<dd>Mods, including x-ray texture packs that grant a tactical advantage are strictly forbidden. <em>This does not apply to OptiFine.</em></dd>
+		
+		<dt><div class="num">5</div> Do not interfere with core services</dt>
+		<dd>This includes tearing up or tampering with footpaths, canals and minecart tracks.</dd>
+	</dl>
+	
+	<div class="section-header" id="moderators">
+  		<h1>Moderation Team</h1>
+  	</div>
+  		<p>Moderators are <strong>volunteers</strong> who have agreed to assist in keeping the server community healthy and happy. They have been granted the power to kick or ban players who break the rules, and roll back block changes in cases of griefing. Currently, there are two moderators: <strong>mrxak</strong> and <strong>mud212</strong>. Please do not hesitate to contact them if you need any assistance.</p>
+  		
+  		<div class="section-header" id="admins">
+  			<h1>Administration Team</h1>
+  		</div>
+  		<p>m.neffy.me is a labor of love. The administration team, like the moderators, have volunteered their time to make sure the server remains a pleasant experience. The current administrator team consists of:</p>
+	
+  		<dl>
+  			
+  			<dt>nfreader</dt>
+  			<dd>handles day-to-day administration tasks</dd>
+  			
+  			<dt>prophile</dt>
+  			<dd>oversees all technical aspects of the server software and related services</dd>
+
+  		</dl>
+  		
+  		<div class="section-header" id="help">
+  			<h1>Getting Help</h1>
+  		</div>
+  		<p>The best way to get help with an issue may depend on what the issue is.</p>
+  		<dl>
+	<dt>For active, ongoing griefing</dt>
+	<dd>Please contact a member of the moderation team in-game, or if none are available, contact us via <a href="chat.php">HipChat</a></dd>
+	
+	<dt>For recently discovered griefing</dt>
+	<dd>Ask an in-game moderator to rollback the changes.</dd>
+		
+	<dt>For technical issues</dt>
+	<dd>If the server is down or you are otherwise unable to connect, please contact us via <a href="chat.php">HipChat</a>, or email <a href="mailto:minecraft@nfreader.net">minecraft@nfreader.net</a>
+	</dd>
+	
+	<dt>For all other issues</dt>
+	<dd>If the issue is in-game, contact an active moderator in game. Otherwise, contact us via <a href="chat.php">HipChat</a></dd>	
+	</dl>
+	
+	<div class="section-header" id="tech">
+		<h1>Technology</h1>
+	</div>
+	
+	<p>The server runs on a <a href="https://www.digitalocean.com/?refcode=e8fee75698ff">DigitalOcean Cloud Server</a><small>(referral link)</small> with 2GB of RAM and dual cores. Thanks to the work of <a href="#admins">prophile</a>, the server is set up and provisioned with <a href="https://github.com/prophile/glory">Glory</a>. Management is handled through <a href="https://github.com/mcdevs/mark2">mark2</a>.</p>
+	
+	<iframe src="http://ghbtns.com/github-btn.html?user=prophile&repo=glory&type=fork&count=true&size=large"
+  allowtransparency="true" frameborder="0" scrolling="0" width="95" height="30"></iframe>
+
+	<div class="section-header" id="contribute">
+		<h1>Contribute</h1>
+	</div>
+	
+	<p>There are two ways to help make sure the server and the experience you enjoy run as smoothly as possible.</p>
+	<dl>
+	<dt>Financial contributions</dt>
+  	<dd>If you are over the age of 18, you can help the server by contributing through PayPal or with Bitcoin.</dd>
+	
+	<dt>Code contributions</dt>
+	<dd>The tools powering the server are open source and publicly available on GitHub. If you are so inclined, feel free to correct any errors you find and issue pull requests.</dd>
+</dl>
+<?php require_once('footer.php'); ?>
