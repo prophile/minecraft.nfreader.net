5a98524fe2ad7aa2fddf4f195ba29e8c71bfc3b3
Nick Farley <nick@nfreader.net>
2014-01-22 16:36:31 -0500

    Responsive
    Site will convert to mobile-friendly view if the viewport is narrower
    than 1024 pixels.

--- map.php
+++ map.php
@@ -1,5 +1,5 @@
 <?php require_once('header.php');?>
 
-<iframe src="http://minecraft.nfreader.net/map/" seamless="true" style="position: absolute; right:0px; bottom: 0px; width: 100%; height: 100%;"></iframe>
+<iframe src="http://minecraft.nfreader.net/map/" seamless="true" class="map"></iframe>
 
 <?php require_once('footer.php')?>
